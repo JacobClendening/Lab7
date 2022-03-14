0e5dbc1032ee596108a756d5efabcb3d571261c3
Jacob Clendening <S3534180@students.fscj.edu>
2022-03-13 17:12:36 -0700

    added error exceptions to backup code.

--- Lab7Script.py
+++ Lab7Script.py
@@ -5,6 +5,7 @@ import netmiko
 from netmiko import ConnectHandler
 from getpass import getpass
 from pathlib import Path
+from netmiko.ssh_exception import AuthenticationException, SSHException, NetMikoTimeoutException
 
 username = input("Username: ")
 password=getpass("Enter your password: ")
@@ -24,10 +25,14 @@ router = {
     'password': password
 }
 
-c = ConnectHandler(**router)
-
-output = c.send_command('show run')
-
-openFile.write(output)
-
-openFile.close()
+try:
+    c = ConnectHandler(**router)
+    output = c.send_command('show run')
+    openFile.write(output)
+    openFile.close()
+except (AuthenticationException):
+    print("An authentication error occured while connecting to: " + routerIP)
+except (SSHException):
+    print("An error occured while connecting to device " + routerIP + " via ssh. Is it enabled?")
+except (NetMikoTimeoutException):
+    print("The device " + routerIP + "timed out when attempting to connect. Is the device online?")
